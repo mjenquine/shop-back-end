90107d402c74dba35b83650ac7aae150585a4845
vksharma <vikram.sharma1024@gmail.com>
2020-03-16 19:42:05 -0400

    committing one-model API

---
+++ server.js
@@ -0,0 +1,45 @@
+//dependencies
+const express = require('express')
+const mongoose = require('mongoose')
+const cors = require('cors')
+const app = express()
+PORT = 3003
+
+//middleware
+
+app.use(express.json())
+
+const whitelist = ['http://localhost:3000']
+const corsOptions = {
+    origin: function (origin, callback){
+        if(whitelist){
+            callback(null, true)
+        }else{
+            callback(newError('Not Allowed In CORS'))
+        }
+    }
+}
+// app.use(cors(corsOptions))
+
+mongoose.connection.on('error', err =>
+console.log(err.message + 'is Mongo not running?'))
+mongoose.connection.on('disconnected', () => {
+        console.log('mongo disconnected');
+})
+mongoose.connect('mongodb://localhost:27017/shop', { useUnifiedTopology: true, useNewUrlParser: true})
+mongoose.connection.once('open', () => {
+    console.log('Mongoose is currently 🎊🎊 POPPING');
+})
+
+//controllers and routes
+app.get('/', (req, res) => {
+    res.send('Welcome to SHOP')
+})
+
+const shopController = require('./controllers/shop.js')
+app.use('/shop', shopController)
+
+//listener
+app.listen(PORT, () => {
+    console.log('A-okay 👌 on port: ', PORT);
+})
