8c4d4193c730f6a9341b25b6895ad513d8db15f3
Mark Jenquine <mjenquine@gmail.com>
2020-03-17 17:45:45 -0500

    edited schema

--- models/item.js
+++ models/item.js
@@ -2,8 +2,9 @@ const mongoose = require('mongoose')
 
 const itemSchema = mongoose.Schema({
     name: String,
-    price: Number,
-    description: String
+    price: String,
+    description: String,
+    img: String
 })
 
 module.exports = mongoose.model('Item', itemSchema)
