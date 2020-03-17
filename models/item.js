7562eaf401738e447b29a06fb9836908ec001590
Eric <outboundflavor@gmail.com>
2020-03-17 18:39:27 -0500

    added schema alterations

--- models/item.js
+++ models/item.js
@@ -1,10 +1,10 @@
 const mongoose = require('mongoose')
 
 const itemSchema = mongoose.Schema({
-    name: String,
-    price: String,
-    description: String,
-    img: String
+    name: {type: String},
+    price: {type: String},
+    description: {type: String},
+    img: {type: String}
 })
 
 module.exports = mongoose.model('Item', itemSchema)
